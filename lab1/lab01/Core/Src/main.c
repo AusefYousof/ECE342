a67a6902d29ecd32fa3381f4f929d0e99accb0a3
AusefYousof <ausef.yousof@mail.utoronto.ca>
2024-03-05 15:20:02 -0500

    final labs commit

--- lab1/lab01/Core/Src/main.c
+++ lab1/lab01/Core/Src/main.c
@@ -13,7 +13,7 @@
   * in the root directory of this software component.
   * If no LICENSE file comes with this software, it is provided AS-IS.
   *
-  ******************************************************************************
+  *******************************************************************************
   */
 #include "main.h"
 #include "config.h"
